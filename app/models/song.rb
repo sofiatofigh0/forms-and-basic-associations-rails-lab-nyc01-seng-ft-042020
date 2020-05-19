309ec6e0f1a1ab70dd8d646d3aeb9d08fb997682
Sofia Tofigh <sofiatofigh0@gmail.com>
2020-05-18 23:06:05 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,14 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist
+  end
+  
+
 end
